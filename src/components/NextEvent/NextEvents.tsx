b6f16cc43b03a572feb3eb3bea8a48b25dd8c435
Luca <corbuccilu@gmail.com>
2023-10-22 15:28:25 +0200

    ✨ Adds an if to check if there are new events

--- src/components/NextEvent/NextEvents.tsx
+++ src/components/NextEvent/NextEvents.tsx
@@ -1,4 +1,3 @@
-import { Dict } from "@trpc/server";
 import { Events } from "../../const/Events";
 import { useEffect, useState } from "react";
 import Link from "next/link";
@@ -13,7 +12,7 @@ type EventDetails = {
 	day: string;
 	month: string;
 	year: string;
-    time?: string;
+	time?: string;
 };
 
 export const NextEvents = () => {
@@ -49,7 +48,7 @@ export const NextEvents = () => {
 							day: event.day,
 							month: event.month,
 							year: event.year,
-                            time: event.time,
+							time: event.time,
 						});
 					}
 				});
@@ -61,55 +60,75 @@ export const NextEvents = () => {
 		}
 	}, [loading]);
 
-	console.log(current_day, current_month, current_year);
-	return (
+	return loading ? (
+		<div>loading ... </div>
+	) : eventsList.length == 0 ? (
+		<div></div>
+	) : (
 		<div className="bg-white">
 			<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-				<h2 className="text-2xl font-bold tracking-tight text-gray-900">
-					I prossimi eventi:
-				</h2>
-
+				<div className="flex flex-col text-center w-full mb-20">
+					<h1 className="sm:text-3xl text-2xl font-bold title-font text-gray-900">
+						I nostri prossimi eventi
+					</h1>
+				</div>
 				<div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-					{loading ? (
-						<div>loading ... </div>
-					) : (
-						eventsList.map((event) => (
-							<div key={"test3"} className="group relative" style={{ display: 'flex', flexDirection: 'column' }}>
-								<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
-									<img
-										src={event.imageUrl}
-										className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-									/>
-								</div>
+					{eventsList.map((event) => (
+						<div
+							key={"test3"}
+							className="group relative"
+							style={{ display: "flex", flexDirection: "column" }}
+						>
+							<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+								<img
+									src={event.imageUrl}
+									className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+								/>
+							</div>
 
-                                
-								<div className="mt-4 justify-between " style={{ flexGrow: 50 }}>
-									<a className="mt-4 justify-between text-xl font-bold text-gray-700">
-										{event.title}
-									</a>
-								</div>
+							<div
+								className="mt-4 justify-between "
+								style={{ flexGrow: 50 }}
+							>
+								<a className="mt-4 justify-between text-xl font-bold text-gray-700">
+									{event.title}
+								</a>
+							</div>
 
-                                <div className="relative flex py-2 items-center">
-                                    <div className="flex-grow border-t border-transparent"></div>
-                                    <div className="flex-grow border-t border-transparent"></div>
-                                </div>
+							<div className="relative flex py-2 items-center">
+								<div className="flex-grow border-t border-transparent"></div>
+								<div className="flex-grow border-t border-transparent"></div>
+							</div>
 
-
-								<div className="mt-4 justify-between" style={{ flexGrow: 1 }}>
-									<a className="mt-4 justify-between text-xl font-bold text-gray-700">
-										{event.time
+							<div
+								className="mt-4 justify-between"
+								style={{ flexGrow: 1 }}
+							>
+								<a className="mt-4 justify-between text-xl font-bold text-gray-700">
+									{event.time
 										? new Date(
 												parseInt(event.year),
 												parseInt(event.month) - 1,
 												parseInt(event.day),
-                                                parseInt(event.time.slice(0, event.time.indexOf(":"))),
-                                                parseInt(event.time.slice(event.time.indexOf(":")+1)),
+												parseInt(
+													event.time.slice(
+														0,
+														event.time.indexOf(":"),
+													),
+												),
+												parseInt(
+													event.time.slice(
+														event.time.indexOf(
+															":",
+														) + 1,
+													),
+												),
 										  ).toLocaleString("it-IT", {
 												day: "2-digit",
 												year: "numeric",
 												month: "long",
-                                                hour: "numeric",
-                                                minute: "numeric",
+												hour: "numeric",
+												minute: "numeric",
 										  })
 										: new Date(
 												parseInt(event.year),
@@ -120,19 +139,18 @@ export const NextEvents = () => {
 												year: "numeric",
 												month: "long",
 										  })}
-									</a>
-								</div>
-
-								<div className="mt-4 justify-between" >
-									<Link href={event.href}>
-										<a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 md:py-4 md:text-lg md:px-10">
-											Registrati
-										</a>
-									</Link>
-								</div>
+								</a>
 							</div>
-						))
-					)}
+
+							<div className="mt-4 justify-between">
+								<Link href={event.href}>
+									<a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 md:py-4 md:text-lg md:px-10">
+										Registrati
+									</a>
+								</Link>
+							</div>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
