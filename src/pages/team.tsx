ff09337b6f620bc97faa4af6bbcdfc2c69a661a4
micheledamiano <49484465+micheledamiano@users.noreply.github.com>
2023-11-07 22:52:46 +0100

    Update team.tsx - Removed class: overflow-hidden

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -6,7 +6,7 @@ import { Team } from "../components/Team";
 const Mentorship: NextPage = () => {
 	return (
 		<div>
-			<div className="relative pt-6 pb-16 sm:pb-24 overflow-hidden">
+			<div className="relative pt-6 pb-16 sm:pb-24">
 				<HeaderBase />
 			</div>
 			<Team />
